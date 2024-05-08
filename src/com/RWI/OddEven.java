9d956c49ea29eca500ddb5dbd78c535627571301
ParulSingh2001 <Parulsingh1717@gmail.com>
2024-05-08 08:19:47 +0530

    Initial version

--- src/com/RWI/OddEven.java
+++ src/com/RWI/OddEven.java
@@ -1,4 +1,12 @@
 package com.RWI;
-
+interface start{
+    public  void show(int x);
+}
 public class OddEven {
+    public static void main(String[] args){
+        start s1=x-> {
+            System.out.println(x%2==0);
+        };
+        s1.show(19);
+    }
 }
